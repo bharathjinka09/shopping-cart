42d32a8c7075c4773f996c9ee3af719660e0543c
bharathjinka09 <bharathjinka09@gmail.com>
2020-05-01 22:18:49 +0530

    Updates

---
+++ precache-manifest.6af010c29476c041c20d3e9ff7911e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00fd8b00e6a85757da0e1eeaf602d66b",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "95c1745f4e535a064c9c",
+    "url": "/shopping-cart/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "2d81aa7b8128baaa69c4",
+    "url": "/shopping-cart/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "95c1745f4e535a064c9c",
+    "url": "/shopping-cart/static/js/2.2652018f.chunk.js"
+  },
+  {
+    "revision": "2d81aa7b8128baaa69c4",
+    "url": "/shopping-cart/static/js/main.a6e2e69d.chunk.js"
+  },
+  {
+    "revision": "3675f54918f9d621420d",
+    "url": "/shopping-cart/static/js/runtime~main.60a85d2f.js"
+  }
+]);
